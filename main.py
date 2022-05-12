f13ffa1815a3155f08480935815e4eb128bd8148
vincenzoraiano <42638331+vincenzoraiano@users.noreply.github.com>
2022-05-12 12:06:05 +0200

    Create main.py

---
+++ main.py
@@ -0,0 +1,22 @@
+print("seleziona il size")
+
+size=int(input())
+
+print(size)
+
+f=open("vector_a", "w")
+f1=open("vector_b","w")
+
+f.write("n="+str(size)+"\n")
+
+
+for x in range (size):
+    str(size)
+    f.write(str(1)+"\n")
+    f1.write(str(1)+"\n")
+
+f.close()
+f1.close()
+
+
+
